a7a38bc4ed2e244914b0a5845dae09cee123a212
Arup Parida <arupggn@gmail.com>
2023-05-01 19:00:13 +0530

    All new changes for the day

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,9 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult About(){
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
